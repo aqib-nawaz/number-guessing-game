c786c2b8491fc4ca67df0776b6afb3f5201263db
Aqib Nawaz <aqibnawaz032@gmail.com>
2022-04-28 14:24:59 +0500

    category selection logic

--- app.js
+++ app.js
@@ -6,3 +6,29 @@ let message = document.getElementById('message')
 let category = document.getElementById('select-category');
 
 let button = document.getElementById('check-button')
+
+button.disabled = true;
+
+let onCategorySelect = () => {
+    
+    let categoryVal = category.value;
+    // console.log(typeof categoryVal)
+
+    button.disabled = false;
+
+
+    if(categoryVal === "easy"){
+        turns.textContent = 7;
+
+    } else if(categoryVal == "medium"){
+        turns.textContent = 5;
+
+    } else if( categoryVal === "hard"){
+        turns.textContent = 3;
+
+    } else {
+        turns.textContent = "";
+        button.disabled = true;
+    }
+}
+category.onchange = onCategorySelect;
